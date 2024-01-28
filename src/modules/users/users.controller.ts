13346c0bfa839a909545a54e718f9ea4108d1d72
Felix <fdbatista@gmail.com>
2024-01-28 12:56:07 +0100

    #3 Create users controller

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, HttpCode, HttpStatus, Get, Param, UseGuards } from '@nestjs/common';
+import { FindOneDTO } from '../../common/dtos/find-one.dto';
+
+import { Reflector } from '@nestjs/core';
+import { RolesGuard } from '../app/http/guards/authorization.guard';
+export const Roles = Reflector.createDecorator<string[]>();
+
+@Controller({
+  path: 'users',
+  version: '1',
+})
+export class UsersController {
+  constructor() {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  @UseGuards(RolesGuard)
+  @Roles(['admin'])
+    findOne(@Param() params: FindOneDTO) {
+    const { id } = params;
+    return { id };
+  }
+}
