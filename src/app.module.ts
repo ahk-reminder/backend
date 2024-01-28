98e13f39fa8e451081c812773bce81b5bbd66e49
Felix <fdbatista@gmail.com>
2024-01-28 10:27:58 +0100

    #2 Register user and auth modules in main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,16 @@ import { AppController } from './http/controllers/app.controller';
 import { AppService } from './services/app.service';
 import { LoggerMiddleware } from './http/middleware/logger.middleware';
 import { JwtAuth } from './http/middleware/jwt-auth.middleware';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware, JwtAuth)
-      .forRoutes(AppController);
+    consumer.apply(LoggerMiddleware, JwtAuth).forRoutes('*');
   }
 }
--- src/http/controllers/app.controller.ts
+++ src/http/controllers/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from '../../services/app.service';
 
+import { Reflector } from '@nestjs/core';
+import { RolesGuard } from '../guards/authorization.guard';
+export const Roles = Reflector.createDecorator<string[]>();
+
 @Controller()
+@UseGuards(RolesGuard)
+@Roles(['admin'])
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
