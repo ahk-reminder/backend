76b84be7d7e3f87d396b90114efb82c061a2ec4c
Felix <fdbatista@gmail.com>
2024-01-28 19:23:25 +0100

    #3 Register new auth module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -7,10 +7,12 @@ import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthenticationModule } from '../authentication/authentication.module';
 
 @Module({
   imports: [
     AuthModule,
+    AuthenticationModule,
     UsersModule,
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
@@ -30,6 +32,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware, JwtAuth).forRoutes('*');
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(JwtAuth).exclude('(.*)/login').forRoutes('*');
   }
 }
