b5b1bc43a1b139e70565cf5d087f9da809ba0d0d
Felix <fdbatista@gmail.com>
2024-01-28 19:27:16 +0100

    #3 Move LoginDTO into authentication module

--- src/modules/authentication/authentication.controller.ts
+++ src/modules/authentication/authentication.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
-import { LoginDto } from '../auth/dtos/login.dto';
+import { LoginDto } from './dtos/login.dto';
 
 @Controller('authentication')
 export class AuthenticationController {
