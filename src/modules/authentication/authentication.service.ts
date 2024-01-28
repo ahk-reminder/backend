882ddaf8e2fca568ba96f9e34ad6d504a94f652f
Felix <fdbatista@gmail.com>
2024-01-28 20:12:05 +0100

    #3 Refactor auth service to generate JWT tokens

--- src/modules/authentication/authentication.service.ts
+++ src/modules/authentication/authentication.service.ts
@@ -1,17 +1,27 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthenticationService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(username: string, password: string): Promise<any> {
+  async login(
+    username: string,
+    password: string,
+  ): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(username);
 
     if (user?.password !== password) {
       throw new UnauthorizedException();
     }
 
-    return user;
+    const payload = { sub: user.userId, username: user.username };
+    const token = await this.jwtService.signAsync(payload);
+
+    return { access_token: token };
   }
 }
