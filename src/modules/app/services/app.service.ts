3a39c24d73b7863f66cec34e504089a1f0281690
Felix <fdbatista@gmail.com>
2024-01-28 12:57:26 +0100

    #3 Rename app controller and service methods

--- src/modules/app/services/app.service.ts
+++ src/modules/app/services/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EMPTY_STRING } from '../../../common/utils/constants';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private configService: ConfigService) {}
+  
+  getAppVersion(): string {
+    return this.configService.get<string>('APP_VERSION') ?? EMPTY_STRING;
   }
 }
