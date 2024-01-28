b7484adbd8c00fee1f147fa16cf4a86003e814d9
Felix <fdbatista@gmail.com>
2024-01-28 19:23:39 +0100

    #3 Reformat JWT middleware

--- src/modules/app/http/middleware/jwt.middleware.ts
+++ src/modules/app/http/middleware/jwt.middleware.ts
@@ -4,7 +4,6 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class JwtAuth implements NestMiddleware {
   use(request: Request, response: Response, next: NextFunction) {
-    
     console.log('JWT is valid...');
     next();
   }
