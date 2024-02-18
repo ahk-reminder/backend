864d757841d4cb6cefac8c5c34a9964d2ca47057
Felix <fdbatista@gmail.com>
2024-02-18 22:30:53 +0100

    #6 Register user repository as provider

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from 'src/data-access/entities/user.entity';
+import { UserRepository } from 'src/data-access/repositories/user.repository';
 
 @Module({
-  providers: [UsersService],
+  imports: [SequelizeModule.forFeature([User])],
+  providers: [UsersService, UserRepository],
   exports: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
