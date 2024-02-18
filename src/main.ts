537ab89f731975bf568a3ab138c91764807b06ed
Felix <fdbatista@gmail.com>
2024-02-18 17:48:41 +0100

    #5 Load app port from `.env` file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config({
+    path: `.env.${process.env.NODE_ENV || 'development'}`,
+  });
+
   const app = await NestFactory.create(AppModule);
 
   app.enableVersioning({
@@ -10,8 +15,10 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const appPort = parseInt(process.env.APP_PORT || '3000')
   
-  await app.listen(3000);
+  await app.listen(appPort);
 }
 
 bootstrap();
