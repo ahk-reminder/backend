7c7377f8fb6b82b285dfe29c5276c053af183ff9
Felix <fdbatista@gmail.com>
2024-02-18 22:29:03 +0100

    #6 Config Sequelize

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SequelizeModule } from '@nestjs/sequelize';
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './http/controllers/app.controller';
 import { AppService } from './app.service';
@@ -6,13 +7,28 @@ import { JwtAuth } from './http/middleware/jwt.middleware';
 import { UsersModule } from '../users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { AuthenticationModule } from '../authentication/authentication.module';
+import { User } from 'src/data-access/entities/user.entity';
 
 @Module({
   imports: [
     AuthenticationModule,
     UsersModule,
     ConfigModule.forRoot(),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        models: [User],
+      }),
+      inject: [ConfigService],
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -33,7 +49,10 @@ import { AuthenticationModule } from '../authentication/authentication.module';
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule implements NestModule {
+  constructor(private dataSource: DataSource) { }
+
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
     consumer.apply(JwtAuth).exclude('(.*)/login').forRoutes('*');
--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EMPTY_STRING } from './utils/constants';
+import { Sequelize } from 'sequelize-typescript';
+
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
-  
+  constructor(
+    private configService: ConfigService,
+    private sequelize: Sequelize,
+  ) { }
+
   getAppVersion(): string {
     return this.configService.get<string>('APP_VERSION', EMPTY_STRING);
   }
